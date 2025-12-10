8130d40f8ccd2f356f2c7ad6966667387f98a478
Siddhi Danekar <danekarsiddhi@gmail.com>
2025-12-10 20:34:03 +0530

    Add Sub

---
+++ arithmetic.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    print("a + b", a + b)
+
+def sub(a,b):
+    print("a - b", a - b)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+import arithmetic
 print("Hello, world")
+arithmetic.add(3,10)
+arithmetic.sub(10,3)
