e914b5a046ec4a0be6c75abe68daadf791520034
Siddhi Danekar <danekarsiddhi@gmail.com>
2025-12-10 21:35:50 +0530

    gitignore file

---
+++ geometry.py
@@ -0,0 +1,7 @@
+def rec_area(a, b):
+    print(a * b)
+
+def rec_peri(a,b):
+    print(2 * a * b)
+    
+
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import arithmetic
+import geometry
 print("Hello, world")
 arithmetic.add(3,10)
 arithmetic.sub(10,3)
+geometry.rec_area(2, 4)
+geometry.rec_peri(2,4)
